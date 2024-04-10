b9dce1ea0c1274cc4cf415ba58391487692f508b
Delia Eisenhower <166590257+Eisenhower1986@users.noreply.github.com>
2024-04-10 16:27:57 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    KEY_FILE = 'key.key'
+    ENCRYPTED_FILE_EXTENSION = '.enc'
+
+# Example usage
+if __name__ == '__main__':
+    print("Key file:", Config.KEY_FILE)
+    print("Encrypted file extension:", Config.ENCRYPTED_FILE_EXTENSION)
